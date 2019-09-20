c6408c92db85cf15c2727538ab049f6edae03bf5
errorreplicator <error.replicator@gmail.com>
2019-09-20 19:33:08 +0200

    .

--- conolution/single_img.py
+++ conolution/single_img.py
@@ -3,23 +3,17 @@ import numpy as np
 from PIL import Image
 from matplotlib import pyplot as plt
 
-
 img = Image.open('c:/Dataset/44.jpg').convert('L')
 
 # plt.imshow(np.asarray(img))
 # plt.show()
-# display(img)
+# # # display(img)
 
 print(np.asarray(img).shape)
-# samp_img = np.random.randint(0,255,size=(5,5))
 
 np_img = np.pad(np.asarray(img),1,mode='constant')
-conv = Conv3x3(8)
+conv = Conv3x3(1)
 output = conv.forward(np_img)
-# output = output.reshape(output.shape[0],output.shape[1])
 print(output.shape)
 plt.imshow(output, cmap='gray')
 plt.show()
-# # print(output.shape)
-# # print()
-# # print(output)
--- test.py
+++ test.py
@@ -1,6 +1,15 @@
-import random
-random.seed(192)
+import numpy as np
+from PIL import Image
+from matplotlib import pyplot as plt
 
-print(random.randint(30,60))
+img = Image.open('c:/Dataset/44.jpg').convert('L')
+img = np.asarray(img)
+print(img.shape)
+zm1 = np.random.randn(4,4)/9
+zm2 = [[1,2,1],[0,0,0],[-1,-2,-1]]
 
-# 36* = 40
+zm1 = np.pad(zm1,1,mode='constant')
+
+print(zm1)
+
+
