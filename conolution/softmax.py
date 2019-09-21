0e3b5fc1864c2aeec36e38539bfce4652aa92d62
piotr <error.replicator@gmail.com>
2019-09-21 13:53:31 +0200

    working conv and max pool

---
+++ conolution/softmax.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class Softmax:
+
+    def __init__(self,input_len,nodes):
+        self.weights = np.random.rand(input_len,nodes) / input_len
+        self.bias = np.zeros(nodes)
+
+    def forward(self,input):
+        input = input.flatten()
+
+        input_len, nodes = self.weights.shape
+
+        total = np.dot(input, self.weights) + self.bias
+        exp = np.exp(total)
+        return exp / np.sum(exp, axis=0)
