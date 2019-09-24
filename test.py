b138184e891d2eb462bab023f7cd5af4d032aada
errorreplicator <error.replicator@gmail.com>
2019-09-24 21:35:45 +0200

    just .

--- test.py
+++ test.py
@@ -3,15 +3,15 @@ import mnist
 from matplotlib import pyplot as plt
 np.set_printoptions(linewidth=np.inf)
 
-HOW EXP WIRKS AT THE END
+# HOW EXP WIRKS AT THE END
 
-imgs = mnist.test_images()
-print(len(imgs))
-print(type(mnist))
-print(imgs[0])
+# imgs = mnist.test_images()
+# print(len(imgs))
+# print(type(mnist))
+# print(imgs[0])
 
-plt.imshow(imgs[0])
-plt.show()
+# plt.imshow(imgs[0])
+# plt.show()
 
 
 np.random.seed(20)
@@ -24,14 +24,9 @@ print(img, '->random image after maxpool', end='\n\n')
 img = img.flatten()
 print(img, '->image flatter', end='\n\n')
 
-weights = np.random.randint(0, 25, size=(4, 4))
+weights = np.random.randint(0, 25, size=(4, 4))/10
 print(weights, '-> weights', end='\n\n')
 
 total = np.dot(img,weights) + biases
 print(total, '->dot total + bias',end='\n\n')
 
-exp = np.exp(total) # check exp how it works
-print(exp,end='\n\n')
-
-retu = exp / np.sum(exp,axis=0)
-print(retu)
