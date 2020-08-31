005d450e6c53c37f4f1dc0810dde60a1f8fcf722
errorreplicator <error.replicator@gmail.com>
2020-08-31 13:02:04 +0200

    nauka w

--- test.py
+++ test.py
@@ -1,12 +1,23 @@
-def ile(n):
-    for x in range(1,n+1):
-        if x == 1:
-            start = 1
-        if x%2 == 0:
-            start +=3
-        if x%2 != 0 and x>2:
-            start-=1
-        if x%12==0:
-            start-=9
-    print(start)
-ile(100)
+import PIL as pil
+from PIL import Image, ImageDraw
+
+
+im = Image.new('RGB',[500,500],(255,0,255))
+imDraw = ImageDraw.Draw(im)
+
+imDraw.rectangle([(10,10),(210,110)],fill=(0,0,255),outline=(0,0,0))
+
+im.show()
+
+
+
+# for x in range(3000):
+#     if x<=255:
+#         im = Image.new('RGB',[500,500],(x,0,0))
+#     elif x>255 and x <= 510:
+#         im = Image.new('RGB',[500,500],(255,x-255,0))
+#     elif x>510 and x <=765:
+#         im = Image.new('RGB',[500,500],(255,255,x-765))
+#     else:
+#         im = Image.new('RGB',[500,500],(255,255,255))
+#     im.save(f'c:/1/{x}.png','png')
